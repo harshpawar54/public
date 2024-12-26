63df74d7b2a18039b7b609467febd1f08c392982
harshpawar54 <harshpawar54@gmail.com>
2024-12-26 11:25:59 +0800

    Update SqlParameter.java

--- SqlParameter.java
+++ SqlParameter.java
@@ -1,10 +1,16 @@
 public class SqlParameter {
-    private final int type;  // SQL type (e.g., Types.INTEGER, Types.VARCHAR)
-    private final Object value;  // Value of the parameter
+    private final int type;         // SQL type (e.g., Types.INTEGER, Types.VARCHAR)
+    private final Object value;    // Value of the parameter
+    private final boolean isOutParameter; // Indicates if it's an OUT parameter
 
     public SqlParameter(int type, Object value) {
+        this(type, value, false);
+    }
+
+    public SqlParameter(int type, Object value, boolean isOutParameter) {
         this.type = type;
         this.value = value;
+        this.isOutParameter = isOutParameter;
     }
 
     public int getType() {
@@ -13,5 +19,9 @@ public class SqlParameter {
 
     public Object getValue() {
         return value;
+    }
+
+    public boolean isOutParameter() {
+        return isOutParameter;
     }
 }
